afcf1a292f60a259ea432359b3d393766e4cab1d
lilghtning <lightningdesign@outlook.de>
2023-02-06 20:31:37 +0100

    Initial commit

---
+++ src/main/java/de/lightplugins/lighteconomy/master/Main.java
@@ -0,0 +1,18 @@
+package de.lightplugins.lighteconomy.master;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onLoad() {
+
+    }
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
